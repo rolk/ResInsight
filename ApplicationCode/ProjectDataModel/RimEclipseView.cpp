rolk/ResInsight

commit 707e8c68ab5e3ed8470c88f40660eaa5ac68cccc

(#163) Sort simulation wells by name

ApplicationCode/ProjectDataModel/RimEclipseView.cpp
@@ -1138,6 +1138,8 @@ void RimEclipseView::syncronizeWellsWithResults()
     {
         this->wellCollection()->wells()[wIdx]->setReservoirView(this);
     }
+
+    this->wellCollection()->sortWellsByName();
 }
 
 //--------------------------------------------------------------------------------------------------

ApplicationCode/ProjectDataModel/RimEclipseWellCollection.cpp
@@ -333,3 +333,16 @@ void RimEclipseWellCollection::calculateIsWellPipesVisible(size_t frameIndex)
         m_isWellPipesVisible[frameIndex][i] = wells[i]->calculateWellPipeVisibility(frameIndex);
     }
 }
+
+bool lessEclipseWell(const caf::PdmPointer<RimEclipseWell>& w1,  const caf::PdmPointer<RimEclipseWell>& w2)
+{
+    return (w1->name() < w2->name());
+}
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
+void RimEclipseWellCollection::sortWellsByName()
+{
+   std::sort(wells.begin(), wells.end(), lessEclipseWell);
+}

ApplicationCode/ProjectDataModel/RimEclipseWellCollection.h
@@ -104,13 +104,15 @@ public:
 
     caf::PdmChildArrayField<RimEclipseWell*>     wells;
 
-    RimEclipseWell*                            findWell(QString name);
+    RimEclipseWell*                     findWell(QString name);
     bool                                hasVisibleWellCells();
     bool                                hasVisibleWellPipes();
+    void                                sortWellsByName();
 
     const std::vector<cvf::ubyte>&      isWellPipesVisible(size_t frameIndex);       
     void                                scheduleIsWellPipesVisibleRecalculation();
 
+protected:
     virtual void                        fieldChangedByUi(const caf::PdmFieldHandle* changedField, const QVariant& oldValue, const QVariant& newValue);
     virtual void                        defineUiOrdering(QString uiConfigName, caf::PdmUiOrdering& uiOrdering);
     virtual caf::PdmFieldHandle*        objectToggleField();

commit 5e20e8be01bf9b6dbb23797fa70b839579f369d0

(#707) Fixed regression regarding well pipe and well cells Introduced in 707e8c6 and not quite fixed in 179f0c9

ApplicationCode/ProjectDataModel/RimEclipseView.cpp
@@ -1095,7 +1095,7 @@ void RimEclipseView::syncronizeWellsWithResults()
     for (size_t wIdx = 0; wIdx < this->wellCollection()->wells().size(); ++wIdx)
     {
         RimEclipseWell* well = this->wellCollection()->wells()[wIdx];
-        well->setWellResults(NULL);
+        well->setWellResults(NULL, -1);
     }
 
     // Find corresponding well from well result, or create a new
@@ -1111,7 +1111,7 @@ void RimEclipseView::syncronizeWellsWithResults()
         }
         newWells.push_back(well);
 
-        well->setWellResults(wellResults[wIdx].p());
+        well->setWellResults(wellResults[wIdx].p(), wIdx);
     }
 
     // Delete all wells that does not have a result
@@ -1139,13 +1139,6 @@ void RimEclipseView::syncronizeWellsWithResults()
     }
 
     this->wellCollection()->sortWellsByName();
-
-    for (size_t wIdx = 0; wIdx < this->wellCollection()->wells().size(); ++wIdx)
-    {
-        this->wellCollection()->wells()[wIdx]->setWellIndex(wIdx);
-    }
-
-
 }
 
 //--------------------------------------------------------------------------------------------------

ApplicationCode/ProjectDataModel/RimEclipseWell.cpp
@@ -50,7 +50,7 @@ RimEclipseWell::RimEclipseWell()
     name.uiCapability()->setUiHidden(true);
     name.uiCapability()->setUiReadOnly(true);
 
-    m_wellIndex = cvf::UNDEFINED_SIZE_T;
+    m_resultWellIndex = cvf::UNDEFINED_SIZE_T;
 
     m_reservoirView = NULL;
 }
@@ -254,9 +254,17 @@ void RimEclipseWell::defineUiOrdering(QString uiConfigName, caf::PdmUiOrdering& 
 //--------------------------------------------------------------------------------------------------
 bool RimEclipseWell::isWellPipeVisible(size_t frameIndex)
 {
-    CVF_ASSERT(m_wellIndex != cvf::UNDEFINED_SIZE_T);
+    CVF_ASSERT(m_resultWellIndex != cvf::UNDEFINED_SIZE_T);
 
     // Return the possibly cached value
-    return m_reservoirView->wellCollection()->isWellPipesVisible(frameIndex)[m_wellIndex];
+    return m_reservoirView->wellCollection()->isWellPipesVisible(frameIndex)[m_resultWellIndex];
+}
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
+void RimEclipseWell::setWellResults(RigSingleWellResultsData* wellResults, size_t resultWellIndex)
+{
+    m_wellResults = wellResults; m_resultWellIndex = resultWellIndex;
 }
 

ApplicationCode/ProjectDataModel/RimEclipseWell.h
@@ -47,11 +47,11 @@ public:
     virtual ~RimEclipseWell();
     
     void                                setReservoirView(RimEclipseView* ownerReservoirView);
-    void                                setWellIndex(size_t val) { m_wellIndex = val; }
 
-    void                                setWellResults(RigSingleWellResultsData* wellResults) { m_wellResults = wellResults;}
+    void                                setWellResults(RigSingleWellResultsData* wellResults, size_t resultWellIndex);
     RigSingleWellResultsData*           wellResults() { return m_wellResults.p(); }
-    
+    size_t                              resultWellIndex() { return m_resultWellIndex; }
+
     bool                                isWellPipeVisible(size_t frameIndex);
 
     bool                                calculateWellPipeVisibility(size_t frameIndex);
@@ -76,7 +76,7 @@ public:
 
 private:
     cvf::ref<RigSingleWellResultsData>  m_wellResults;
-    size_t                              m_wellIndex;
+    size_t                              m_resultWellIndex;
 
     RimEclipseView*                   m_reservoirView;
 };

ApplicationCode/ProjectDataModel/RimEclipseWellCollection.cpp
@@ -330,7 +330,7 @@ void RimEclipseWellCollection::calculateIsWellPipesVisible(size_t frameIndex)
     
     for (size_t i = 0; i < wells().size(); ++i)
     {
-        m_isWellPipesVisible[frameIndex][i] = wells[i]->calculateWellPipeVisibility(frameIndex);
+        m_isWellPipesVisible[frameIndex][wells[i]->resultWellIndex()] = wells[i]->calculateWellPipeVisibility(frameIndex);
     }
 }
 