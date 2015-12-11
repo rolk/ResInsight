rolk/ResInsight

commit 28a0e1107c87e8e91b4fa1c54b25192ff4864e41

Improved result accessor factory
Moved clamping of time step to zero for static results into factory

ApplicationCode/ProjectDataModel/RimWellLogExtractionCurve.cpp
@@ -213,14 +213,11 @@ void RimWellLogExtractionCurve::updatePlotData()
                 tvDepthValues = eclExtractor->trueVerticalDepth();
             }
 
-            RifReaderInterface::PorosityModelResultType porosityModel = RigCaseCellResultsData::convertFromProjectModelPorosityModel(m_eclipseResultDefinition->porosityModel());
-            m_eclipseResultDefinition->loadResult();
-
             cvf::ref<RigResultAccessor> resAcc = RigResultAccessorFactory::createResultAccessor(
-                eclipseCase->reservoirData(), 0,
-                porosityModel,
+                eclipseCase->reservoirData(),
+                0,
                 m_timeStep,
-                m_eclipseResultDefinition->resultVariable());
+                m_eclipseResultDefinition);
 
             if (resAcc.notNull())
             {

ApplicationCode/ReservoirDataModel/RigResultAccessorFactory.cpp
@@ -30,6 +30,8 @@
 #include "RigMainGrid.h"
 #include "RigResultAccessor.h"
 
+#include "RimEclipseResultDefinition.h"
+
 #include "cvfAssert.h"
 #include "cvfBase.h"
 #include "cvfLibCore.h"
@@ -145,7 +147,34 @@ cvf::ref<RigResultAccessor> RigResultAccessorFactory::createResultAccessor(RigCa
         return NULL;
     }
 
-    return createResultAccessor(eclipseCase, gridIndex, porosityModel, timeStepIndex, scalarSetIndex);
+    size_t adjustedTimeStepIndex = timeStepIndex;
+    if (resultType == RimDefines::STATIC_NATIVE)
+    {
+        adjustedTimeStepIndex = 0;
+    }
+
+    return createResultAccessor(eclipseCase, gridIndex, porosityModel, adjustedTimeStepIndex, scalarSetIndex);
+}
+
+//--------------------------------------------------------------------------------------------------
+/// 
+//--------------------------------------------------------------------------------------------------
+cvf::ref<RigResultAccessor> RigResultAccessorFactory::createResultAccessor(RigCaseData* eclipseCase, size_t gridIndex, size_t timeStepIndex, RimEclipseResultDefinition* resultDefinition)
+{
+    RifReaderInterface::PorosityModelResultType porosityModel = RigCaseCellResultsData::convertFromProjectModelPorosityModel(resultDefinition->porosityModel());
+    resultDefinition->loadResult();
+
+    size_t adjustedTimeStepIndex = timeStepIndex;
+    if (resultDefinition->hasStaticResult())
+    {
+        adjustedTimeStepIndex = 0;
+    }
+
+    return RigResultAccessorFactory::createResultAccessor(
+        eclipseCase, 0,
+        porosityModel,
+        adjustedTimeStepIndex,
+        resultDefinition->resultVariable());
 }
 
 //--------------------------------------------------------------------------------------------------

ApplicationCode/ModelVisualization/RivCrossSectionPartMgr.cpp
@@ -126,17 +126,10 @@ void RivCrossSectionPartMgr::updateCellResultColor(size_t timeStepIndex)
                 }
                 else
                 {
-                    size_t adjustedTimeStepIndex = timeStepIndex;
-                    if (cellResultColors->hasStaticResult())
-                    {
-                        adjustedTimeStepIndex = 0;
-                    }
-
                     resultAccessor = RigResultAccessorFactory::createResultAccessor(cellResultColors->reservoirView()->eclipseCase()->reservoirData(),
                                                                                     0,
-                                                                                    RigCaseCellResultsData::convertFromProjectModelPorosityModel(cellResultColors->porosityModel()),
-                                                                                    adjustedTimeStepIndex,
-                                                                                    cellResultColors->resultVariable());
+                                                                                    timeStepIndex,
+                                                                                    cellResultColors);
                 }
 
                 RivCrossSectionPartMgr::calculateEclipseTextureCoordinates(m_crossSectionFacesTextureCoords.p(),

ApplicationCode/ModelVisualization/RivReservoirViewPartMgr.cpp
@@ -785,20 +785,11 @@ void RivReservoirViewPartMgr::computePropertyVisibility(cvf::UByteArray* cellVis
                 const double lowerBound = propertyFilter->lowerBound();
                 const double upperBound = propertyFilter->upperBound();
 
-                size_t adjustedTimeStepIndex = timeStepIndex;
-
-                // Set time step to zero for static results
-                if (propertyFilter->resultDefinition()->hasStaticResult())
-                {
-                    adjustedTimeStepIndex = 0;
-                }
-
                 const RimCellFilter::FilterModeType filterType = propertyFilter->filterMode();
 
-                RifReaderInterface::PorosityModelResultType porosityModel = RigCaseCellResultsData::convertFromProjectModelPorosityModel(propertyFilter->resultDefinition()->porosityModel());
                 RigCaseData* eclipseCase = propFilterColl->reservoirView()->eclipseCase()->reservoirData();
 
-                cvf::ref<RigResultAccessor> resultAccessor = RigResultAccessorFactory::createResultAccessor(eclipseCase, grid->gridIndex(), porosityModel, adjustedTimeStepIndex, propertyFilter->resultDefinition->resultVariable(), propertyFilter->resultDefinition->resultType());
+                cvf::ref<RigResultAccessor> resultAccessor = RigResultAccessorFactory::createResultAccessor(eclipseCase, grid->gridIndex(), timeStepIndex, propertyFilter->resultDefinition());
                 CVF_ASSERT(resultAccessor.notNull());
 
                 //#pragma omp parallel for schedule(dynamic)

ApplicationCode/ModelVisualization/RivTextureCoordsCreator.cpp
@@ -39,13 +39,7 @@ RivTextureCoordsCreator::RivTextureCoordsCreator(RimEclipseCellColors* cellResul
     m_quadMapper = quadMapper;
     CVF_ASSERT(quadMapper && eclipseCase );
 
-    size_t resTimeStepIdx = timeStepIndex;
-
-    if (cellResultColors->hasStaticResult()) resTimeStepIdx = 0;
-
-    RifReaderInterface::PorosityModelResultType porosityModel = RigCaseCellResultsData::convertFromProjectModelPorosityModel(cellResultColors->porosityModel());
-
-    m_resultAccessor = RigResultAccessorFactory::createResultAccessor(eclipseCase, gridIndex, porosityModel, resTimeStepIdx, cellResultColors->resultVariable());
+    m_resultAccessor = RigResultAccessorFactory::createResultAccessor(eclipseCase, gridIndex, timeStepIndex, cellResultColors);
 
     cvf::ref<RigPipeInCellEvaluator> pipeInCellEval = new RigPipeInCellEvaluator(cellResultColors->reservoirView()->wellCollection()->isWellPipesVisible(timeStepIndex),
         eclipseCase->gridCellToWellIndex(gridIndex));

ApplicationCode/ReservoirDataModel/RigResultAccessorFactory.h
@@ -21,14 +21,23 @@
 
 #include "RifReaderInterface.h"
 #include "RigResultAccessor.h"
+
 #include "RimDefines.h"
 
 class RigActiveCellInfo;
 class RigGridBase;
 
+class RimEclipseResultDefinition;
+
 class RigResultAccessorFactory
 {
 public:
+    static cvf::ref<RigResultAccessor>
+        createResultAccessor(RigCaseData* eclipseCase,
+        size_t gridIndex,
+        size_t timeStepIndex,
+        RimEclipseResultDefinition* resultDefinition);
+
     static cvf::ref<RigResultAccessor> 
         createResultAccessor(RigCaseData* eclipseCase,
         size_t gridIndex,
@@ -51,8 +60,6 @@ public:
         size_t timeStepIndex,
         size_t resultIndex);
 
-
-
 private:
     static cvf::ref<RigResultAccessor> 
         createNativeResultAccessor(RigCaseData* eclipseCase,
@@ -60,7 +67,6 @@ private:
                                         RifReaderInterface::PorosityModelResultType porosityModel,
                                         size_t timeStepIndex,
                                         const QString& resultName);
-
 
 };
 

commit 739b1d881d7a42a158de460f972fa7778a4fe83c

Fixed up regression introduced in 28a0e1107c87e8e91b4fa1c54b25192ff4864e41
Loading of result is supposed to happen only for
RimWellLogExtractionCurve

ApplicationCode/ProjectDataModel/RimWellLogExtractionCurve.cpp
@@ -229,6 +229,8 @@ void RimWellLogExtractionCurve::updatePlotData()
                 tvDepthValues = eclExtractor->trueVerticalDepth();
             }
 
+            m_eclipseResultDefinition->loadResult();
+
             cvf::ref<RigResultAccessor> resAcc = RigResultAccessorFactory::createResultAccessor(
                 eclipseCase->reservoirData(),
                 0,

ApplicationCode/ReservoirDataModel/RigResultAccessorFactory.cpp
@@ -162,7 +162,6 @@ cvf::ref<RigResultAccessor> RigResultAccessorFactory::createResultAccessor(RigCa
 cvf::ref<RigResultAccessor> RigResultAccessorFactory::createResultAccessor(RigCaseData* eclipseCase, size_t gridIndex, size_t timeStepIndex, RimEclipseResultDefinition* resultDefinition)
 {
     RifReaderInterface::PorosityModelResultType porosityModel = RigCaseCellResultsData::convertFromProjectModelPorosityModel(resultDefinition->porosityModel());
-    resultDefinition->loadResult();
 
     size_t adjustedTimeStepIndex = timeStepIndex;
     if (resultDefinition->hasStaticResult())